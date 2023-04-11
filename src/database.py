a043976f576e148078c66c148a98d3719dc7778b
jsbol <jsbolaris@gmail.com>
2023-04-10 19:21:26 -0700

    Database.py updated

--- src/database.py
+++ src/database.py
@@ -6,26 +6,67 @@ import csv
 
 print("reading movies")
 
-with open("movies.csv", mode="r", encoding="utf8") as csv_file:
-    movies = [
-        {k: v for k, v in row.items()}
-        for row in csv.DictReader(csv_file, skipinitialspace=True)
-    ]
 
-with open("characters.csv", mode="r", encoding="utf8") as csv_file:
-    characters = [
-        {k: v for k, v in row.items()}
-        for row in csv.DictReader(csv_file, skipinitialspace=True)
-    ]
+# define all objects
 
-with open("conversations.csv", mode="r", encoding="utf8") as csv_file:
-    conversations = [
-        {k: v for k, v in row.items()}
-        for row in csv.DictReader(csv_file, skipinitialspace=True)
-    ]
+# character object
+# name and gender can be null
+class character:
+    def __init__(self, character_id: int, name: str, movie_id: int, gender: str, age: int):
+        self.character_id = character_id
+        self.name = (name or None)
+        self.movie_id = movie_id
+        self.gender = (gender or None)
+        self.age = age
+        self.lineCount = 0
 
-with open("lines.csv", mode="r", encoding="utf8") as csv_file:
-    lines = [
-        {k: v for k, v in row.items()}
-        for row in csv.DictReader(csv_file, skipinitialspace=True)
-    ]
+
+# define movie object
+class movie:
+    def __init__(self, movie_id: int, title: str, year: str, imdb_rating: float, imdb_votes: int, raw_script_url: str):
+        self.movie_id = movie_id
+        self.title = title
+        self.year = year
+        self.imdb_rating = imdb_rating
+        self.imdb_votes = imdb_votes
+        self.raw_script_url = raw_script_url
+
+
+# define line object
+class line:
+    def __init__(self, line_id: int, character_id: int, movie_id: int, conversation_id: int,
+                 line_sort: int, line_text: str):
+        self.line_id = line_id
+        self.character_id = character_id
+        self.movie_id = movie_id
+        self.conversation_id = conversation_id
+        self.line_sort = line_sort
+        self.line_text = line_text
+
+
+# define conversation object
+class conversation:
+    def __init__(self, conversation_id: int, character1_id: int, character2_id: int, movie_id: int):
+        self.numOfLines = 0
+        self.conversation_id = conversation_id
+        self.character1_id = character1_id
+        self.character2_id = character2_id
+        self.movie_id = movie_id
+
+
+# define database with all the information
+# fill a dictionary with the information for each relation
+class database:
+    characters = {}  # empty character list
+    with open("characters.csv", mode="r", encoding="utf8") as csv_file:
+        for row in csv.DictReader(csv_file, skipinitialspace=True):
+            curr_char = character(int(row['character_id']), str(row['name']), int(row['movie_id']), str(row['gender']),
+                                  int(row['age']))
+            characters[curr_char.character_id] = curr_char
+
+    movies = {}  # empty movie list
+    with open("movie.csv", mode="r", encoding="utf8") as csv_file:
+        for row in csv.DictReader(csv_file, skipinitialspace=True):
+            curr_movie = movie(int(row['movie_id']), str(row['title']), int(row['year']), float(row['imdb_rating']),
+                               int(row['imdb_votes']), str(row['raw_script_url']))
+            movies[curr_movie.movie_id] = curr_movie
