9bdf50ca9df661f7058b01d08f9e4a47a564c369
jsbol <jsbolaris@gmail.com>
2023-04-17 16:30:01 -0700

    Database.py updated

--- src/api/lines.py
+++ src/api/lines.py
@@ -10,16 +10,18 @@ def get_line(id: str):
     """
     This endpoint returns a line by its identifier id. For each line:
     * 'line_id:' the internal id of the movie.
-    * 'character_id': the character identifier for the character who spoke the line.
+    * 'character_name': the character name for the character who spoke the line.
     * 'movie_id': the movie identifier for the movie the line was in
     * 'conversation_id': the conversation that this line was a part of.
     * 'line_text': text of the line spoken
     """
     curr_line = db.lines.get(id)
     if curr_line:
+        char = db.characters.get(curr_line.c_id)
         json = {
             "line_id": id,
-            "character_id": curr_line.character_id
+            "char_id": char.id,
+            "character_name": char.name,
         }
         return json
     raise HTTPException(status_code=404, detail="Line not found")
